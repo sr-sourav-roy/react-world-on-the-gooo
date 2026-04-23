b52b04b93d1012117841973fb3c5f0d94d1b9644
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-04-23 22:30:12 +0600

    add by world to react to goo...

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,12 +1,18 @@
 import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  console.log(countries);
   return (
     <div>
       <h1>Hello world RSC countries: {countries.length}</h1>
+      <div className="Countries">
+        {countries.map((country) => (
+          <Country key={country.ccn3.ccn3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import "./Country.css";
 
-const Country = () => {
+const Country = ({ country }) => {
+  //   console.log(country.area);
+  const handleButton = () => {
+    console.log("click me");
+  };
   return (
-    <div>
-      <h1>name:</h1>
+    <div className="Country">
+      <img src={country.flags.flags.png} alt="" />
+      <h2>Name:{country.name.common}</h2>
+      <h4>Region:{country.region.region}</h4>
+      <h4>Continents :{country.continents.continents}</h4>
+      {/* <h4>currencies :{country?.currencies.currencies?.EUR}</h4> */}
+      <h4>Population:{country.population.population}</h4>
+      <h4>
+        Area:{country.area.area}{" "}
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}
+      </h4>
+      <button onClick={handleButton}>Not visit</button>
     </div>
   );
 };
 
 export default Country;
-<h1>name:</h1>;
